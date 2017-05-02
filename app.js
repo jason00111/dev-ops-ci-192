6c5c408e265b1662cc7f75c0972cd6498856965d
jason00111 <emberleyjason@gmail.com>
2017-05-02 12:34:21 -0700

    Switch from ajax to html form

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require('express')
 const app = express()
 const bodyParser = require('body-parser')
 
-app.use(bodyParser.text())
+app.use(bodyParser.urlencoded({ extended : true }))
 
 app.use(express.static('public'))
 
